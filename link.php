01e26712ed776f98620c14d3e8ec3aa24a5d6df8
sinhua-w <61674538+sinhua-w@users.noreply.github.com>
2020-04-15 18:04:39 +0800

    Add files via upload

---
+++ link.php
@@ -0,0 +1,23 @@
+<?php
+	$link=mysqli_connect('localhost','root','a200089030989','php2020');
+	$SQL="SELECT *
+   		  FROM employee";
+
+   	if($result=mysqli_query($link,$SQL))
+   	{
+		echo "<table width='1000' border='1'>";
+		echo "<tr><td>No</td><td>Fname</td><td>Minit</td><td>Lname</td><td>Bdate</td><td>Address</td><td>Sex</td><td>Salary</td>";
+
+   		while($row=mysqli_fetch_assoc($result))
+  		{ 
+   			echo "<tr>";
+   			echo "<td>".$row['No']."</td><td>".$row['Fname']."</td><td>".$row['Minit']."</td><td>".$row['Lname']."</td><td>".$row['Bdate']."</td><td>".$row['Address']."</td><td>".$row['Sex']."</td><td>".$row['Salary']."</td><br/>";
+   		}
+   		
+   		echo "</tr></table>";
+   	}
+   	else
+   	{
+   		echo "資料庫讀取失敗";
+   	}
+?>
